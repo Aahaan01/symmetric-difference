03608177cdcfd92cdff86aabbce0b4e699dae69c
Aahaan01 <gargaahaan01@gmail.com>
2024-12-15 14:05:58 +0530

    first commit

---
+++ project.py
@@ -0,0 +1,9 @@
+Set1={"blue", "green"}
+Set2={"blue", "yellow"}
+Set3=Set1.symmetric_difference(Set2)
+print(Set3)
+
+Set4={1,2,3,4,5}
+Set5={1,5,6,7,8,9}
+Set6=Set4.symmetric_difference(Set5)
+print(Set6)
